3057a680e89435744bae7169879656be8fbb437c
2022113318 <Harold114514@gmail.com>
2025-02-26 11:12:07 +0800

    111

--- src/main/java/com/example/testforopenai/Service/LoggingAdvisor.java
+++ src/main/java/com/example/testforopenai/Service/LoggingAdvisor.java
@@ -2,6 +2,7 @@ package com.example.testforopenai.Service;
 
 import com.example.testforopenai.Entity.Bridge;
 import org.springframework.ai.chat.client.RequestResponseAdvisor;
+import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.api.AdvisedRequest;
 
 import java.io.FileWriter;
@@ -9,15 +10,21 @@ import java.io.IOException;
 import java.util.Map;
 
 public class LoggingAdvisor implements RequestResponseAdvisor {
-    private int flag = 0;
     private int number=Bridge.number;
 
     @Override
     public AdvisedRequest adviseRequest(AdvisedRequest request, Map<String, Object> context) {
-
+        PromptChatMemoryAdvisor promptChatMemoryAdvisor = (PromptChatMemoryAdvisor) context.get("chatMemoryAdvisor");
+        System.out.println("///////////////////////////////////////");
         if(number!=Bridge.number){
+            System.out.println("进入更新");
             number = Bridge.number;
+
+            request.systemParams().put("memory",null);
             request.systemParams().put("memory",Bridge.text);
+            if(Bridge.text.indexOf("Jack") != -1){
+                System.out.println("赋值文本中出现了hava");
+            }
         }
 
 
@@ -28,10 +35,14 @@ public class LoggingAdvisor implements RequestResponseAdvisor {
         }
         flag = 1;
         */
-
-
+        System.out.println("///////////////////////////////////////");
+        System.out.println(request);
+        System.out.println("///////////////////////////////////////");
         Object chatmemory = request.systemParams().get("memory");
-        System.out.println("对话记忆：\n" + chatmemory);
+        if(chatmemory.toString().indexOf("Jack") != -1){
+            System.out.println("第二次在记忆中出现hava");
+        }
+//        System.out.println("对话记忆：\n" + chatmemory);
         // 将对话记忆写入文件
         writeStringToFile(chatmemory.toString(), "src/main/resources/ChatMemory/chat.txt");
 //        System.out.println(request);
